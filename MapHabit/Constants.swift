93850d3660d66356419b7997e5997011a73fc3c5
Dhananjaya Somanna <dhananjaya.somanna@varian.com>
2018-10-15 22:10:16 -0400

    Updated Styling

---
+++ MapHabit/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  MapHabit
+//
+//  Created by Dhananjaya Somanna on 10/15/18.
+//  Copyright © 2018 Dhananjaya Somanna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    
+    static let brightOrange     = UIColor(red: 255.0/255.0, green: 69.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let red              = UIColor(red: 255.0/255.0, green: 115.0/255.0, blue: 115.0/255.0, alpha: 1.0)
+    static let orange           = UIColor(red: 255.0/255.0, green: 175.0/255.0, blue: 72.0/255.0, alpha: 1.0)
+    static let blue             = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+    static let lightblue        = UIColor(red: 45.0/255.0, green: 161.0/255.0, blue: 248.0/255.0, alpha: 1.0)
+    static let darkblue         = UIColor(red: 25.0/255.0, green: 145.0/255.0, blue: 235.0/255.0, alpha: 1.0)
+    static let green            = UIColor(red: 91.0/255.0, green: 197.0/255.0, blue: 159.0/255.0, alpha: 1.0)
+    static let darkGrey         = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+    static let veryDarkGrey     = UIColor(red: 13.0/255.0, green: 13.0/255.0, blue: 13.0/255.0, alpha: 1.0)
+    static let lightGrey        = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    static let black            = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let white            = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+}
